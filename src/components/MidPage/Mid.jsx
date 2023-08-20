206a5996f1dea49bc39d8877e21e09a9a9c59aa0
vancillo <yaredbane@gmail.com>
2023-08-20 16:50:52 -0600

    filtered number

--- src/components/MidPage/Mid.jsx
+++ src/components/MidPage/Mid.jsx
@@ -1,15 +1,21 @@
 import { useSelect } from '../../hooks/useSelect.js'
+import { useEffect, useState } from 'react';
 
 const Mid = () => {
-  const { books } = useSelect()
+  const { books } = useSelect();
+  const [filteredBooksCount, setFilteredBooksCount] = useState(0)
+
+  useEffect(() => {
+    setFilteredBooksCount(books.length)
+  }, [books])
 
   return (
     <div>
       <h3 className='text-4xl text-center gap-1'>
-        <span className='text-amber-300'>{books.length}</span> Libros disponibles
+        <span className='text-amber-300'>{filteredBooksCount} Libros disponibles</span> 
       </h3>
     </div>
-  )
+  );
 }
 
-export default Mid
+export default Mid;
