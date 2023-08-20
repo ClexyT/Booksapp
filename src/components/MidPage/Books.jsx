90e9ca9dc584c5e3f6b37cce76ad3e1d4f8a9ad0
ClexyT <zombirt67@gmail.com>
2023-08-20 11:03:24 -0600

    v5.3

--- src/components/MidPage/Books.jsx
+++ src/components/MidPage/Books.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'; 
+  import PropTypes from 'prop-types'; 
 import jsonBooks from "../../mocks/books.json";
 import useSelectedBooks from "../../hooks/useSelectedBooks";
 
