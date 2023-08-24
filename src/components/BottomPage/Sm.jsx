1206b6fc7082b19682160640d2fed3bfeb517acd
ClexyT <zombirt67@gmail.com>
2023-08-24 04:16:30 -0600

    v6.2

---
+++ src/components/BottomPage/Sm.jsx
@@ -0,0 +1,53 @@
+const Sm = () => {
+  return (
+      const Instagram = () => {
+          return (
+              <>
+            <a href={link} className={color}>
+              <img src='../../public/instagram.svg' alt='instagram-logo' className='w-5' />
+            </a>
+            <a href='https://github.com/ClexyT' className='text-gray-300 hover:text-white'>
+              <img src='../../public/github.svg' alt='github-logo' className='w-5' />
+            </a>
+            <a href='https://twitter.com/ClexyT_' className='text-gray-300 hover:text-white'>
+              <img src='../../public/twitter.svg' alt='twitter-logo' className='w-6' />
+            </a>
+  
+     </>
+    )
+  }
+const Github = () => {
+    return (
+      <>
+            <a href='https://www.instagram.com/aguilarr.ans/' className='text-gray-300 hover:text-white'>
+              <img src='../../public/instagram.svg' alt='instagram-logo' className='w-5' />
+            </a>
+            <a href='https://github.com/ClexyT' className='text-gray-300 hover:text-white'>
+              <img src='../../public/github.svg' alt='github-logo' className='w-5' />
+            </a>
+            <a href='https://twitter.com/ClexyT_' className='text-gray-300 hover:text-white'>
+              <img src='../../public/twitter.svg' alt='twitter-logo' className='w-6' />
+            </a>
+  
+     </>
+    )
+  }
+const Twitter = () => {
+    return (
+      <>
+            <a href='https://www.instagram.com/aguilarr.ans/' className='text-gray-300 hover:text-white'>
+              <img src='../../public/instagram.svg' alt='instagram-logo' className='w-5' />
+            </a>
+            <a href='https://github.com/ClexyT' className='text-gray-300 hover:text-white'>
+              <img src='../../public/github.svg' alt='github-logo' className='w-5' />
+            </a>
+            <a href='https://twitter.com/ClexyT_' className='text-gray-300 hover:text-white'>
+              <img src='../../public/twitter.svg' alt='twitter-logo' className='w-6' />
+            </a>
+  
+     </>
+    )
+}
+)
+}
+export default {Sm,Instagram,Github,Twitter}
