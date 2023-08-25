e39df166301c7051b3958852a5e4ac7ee6ce0ba4
ClexyT <zombirt67@gmail.com>
2023-08-24 20:01:14 -0600

    v7

--- public/index.js
+++ public/index.js
@@ -2,7 +2,9 @@ import instagram from "./instagram.svg"
 import github from "./github.svg"
 import twitter from "./twitter.svg"
 import linkedin from "./linkedin.svg"
+import logo from "./logo.svg"
 export {
+    logo,
     instagram,
     github,
     twitter,
